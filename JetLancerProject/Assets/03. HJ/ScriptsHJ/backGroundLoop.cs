a5b87551460245e3f33d154ead9bcdaeb609c361
metacat97 <tkdtkddmlehdanf97@gmail.com>
2023-08-04 16:39:15 +0900

    Feat : Add Character Select UI & move / loop BackGround script

---
+++ JetLancerProject/Assets/03. HJ/ScriptsHJ/backGroundLoop.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backGroundLoop : MonoBehaviour
+{
+    public Transform targetPos;
+    private float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector2.Distance(targetPos.position, transform.position);
+        if(distance < 5f)
+        {
+            Reposition();
+        }
+    }
+
+    private void Reposition()
+    {
+        transform.position = new Vector2(0, 0);
+    }
+}
---
+++ JetLancerProject/Assets/03. HJ/ScriptsHJ/scrollingObject.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollingObject : MonoBehaviour
+{
+    public Transform targetPos; //이동할 위치입니다.
+
+    public float speed = 0.5f;//움직이는 속도입니다.
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(targetPos.position * speed * Time.deltaTime);
+    }
+}
