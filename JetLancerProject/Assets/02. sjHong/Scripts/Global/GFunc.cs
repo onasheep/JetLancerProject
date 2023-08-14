e9bcfc996955d04db5083c15fa0cd6bfa20d95b3
onasheep <94965545+onasheep@users.noreply.github.com>
2023-08-14 10:07:42 +0900

    Style : change sprite filter type billinear to point Sytle : adjust scripts code convention to pascal pattern

--- JetLancerProject/Assets/02. sjHong/Scripts/Global/GFunc.cs
+++ JetLancerProject/Assets/02. sjHong/Scripts/Global/GFunc.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 
 public static partial class GFunc
 {
-    public static bool IsValid<T>(this T component_ ) where T : Component
+    
+
+
+    public static bool IsValid<T>(this T component_) where T : Component
     {
         Component convert = (Component)(component_ as Component);
         bool isInValid = convert == null || convert == default;
@@ -24,24 +27,7 @@ public static partial class GFunc
         return !isInValid;
     }       // IsValid()
 
-    public static IEnumerator PunchScale(Transform tr,float punchTime, float maxSize, float minSize)
-    {
-        float spendTime = 0f;
-        while (spendTime < punchTime)
-        {
-            Debug.Log("1");
-            spendTime  += Time.time;
-            Vector3 scaleChange = new Vector3(maxSize, maxSize, 0f);
-            float y = tr.localScale.y;
-
-            tr.localScale = new Vector3(tr.localScale.x, y * maxSize * Mathf.Sin(spendTime));
-           
-
-            //tr.localScale = Vector3.Lerp(tr.localScale, scaleChange, 0.5f);
 
 
-
-            yield return null;  
-        }
-    }
 }
+
